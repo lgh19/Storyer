a6fcebf22cd7a386a111c1dba188c7d2cb5e4ade
chh147 <chh147@pitt.edu>
2022-10-04 19:37:40 -0400

    fix initial file for group sort testing

--- testing/group-sort-1.py
+++ testing/group-sort-1.py
@@ -2,6 +2,17 @@
 import sys
 
 students = []
+groups = ["A", "B", "C"]
+# a dictionary to hold the students in their preferred groups
+setGroups = {
+    "A": [],
+    "B": [],
+    "C": []
+}
+
+# placeholders, negative for debugging until set
+numStudents = -1
+numGroups = -1
 
 # range of how many students should be in each group
 minGroupSize = 3
@@ -42,24 +53,41 @@ if len(sys.argv) > 1:
     # split by delimiter
     myNums = currLine.split(';', 1)
 
-    print(myNums)
+    numGroups = int(myNums[0])
+    numStudents = int(myNums[1])
 
     # start getting the students from the file
     for line in f:
         #print(line.strip())
-        currStudent = line.strip().split(';', 4)
-        students.append(Student())
+        # split by numGroups, in case 0 should be 3
+        # also convert all elements to int, since starst as string after being read from file
+        currLine = [int(i) for i in line.strip().split(';', numGroups)]
+        currStudent = currLine[0]
+        # access from index 1, stopping before 4
+        currPrefers = currLine[1:4]
+
+        students.append(Student(currStudent, currPrefers))
 
     f.close()
 
-    groups = ["A", "B", "C"]
+    # print debug
+    for student in students:
+        print(student.ID)
+        print(student.prefers)
 
+    # place students into group dict
+    for student in students:
+        setGroups[groups[student.prefers[0]]].append(student.ID)
+    
+    print()
+    print(setGroups)
+
+# endif
 # otherwise if no file was included just use a default arrangement
 else:
-    groups = ["A", "B", "C"]
-
     # number of students for generating in this test program
     numStudents = 6
+    numGroups = len(groups)
 
     # Generates some students with a basic integer ID and single group preference
     #in java: for(int i = 0; i < numStudents; i++)
@@ -76,21 +104,14 @@ else:
         print(student.ID)
         print(student.prefers)
         print()
+    
+    # places students into the dict based on their primary preference
+    for student in students:
+        for group in groups:
+            if student.prefers[0] == group:
+                # for now just adds student ID for a nice dict print
+                # would ideally store the student's key from database instead
+                setGroups[group].append(student.ID)
 
-# a dictionary to hold the students in their preferred groups
-setGroups = {
-    "A": [],
-    "B": [],
-    "C": []
-}
-
-# places students into the above dict based on their primary preference
-for student in students:
-    for group in groups:
-        if student.prefers[0] == group:
-            # for now just adds student ID for a nice dict print
-            # would ideally store the student's key from database instead
-            setGroups[group].append(student.ID)
-
-print()
-print(setGroups)
+    print()
+    print(setGroups)
